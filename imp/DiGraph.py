3ee77f4eaed0a93e2404c3e441beaecc09941fce
ibrahim mjadly <48316131+ibrahim3999@users.noreply.github.com>
2021-12-22 00:14:04 +0200

    Update DiGraph.py

--- imp/DiGraph.py
+++ imp/DiGraph.py
@@ -1,9 +1,4 @@
-import copy
-import random
-import sys
-from typing import List
 from NodeData import NodeData
-from queue import PriorityQueue
 from src.GraphInterface import GraphInterface
 
 
@@ -18,58 +13,44 @@ class DiGraph(GraphInterface):
         self.__edgeSize = 0
         self.__mc = 0
 
-    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-        if node_id1!=node_id2 and node_id2 in self.Edges_out.get(node_id1):
+    def remove_edge(self, node_id1, node_id2):
+        if node_id1 != node_id2 and node_id2 in self.Edges_out.get(node_id1):
             self.Edges_out.get(node_id1).pop(node_id2)
             self.Edges_in.get(node_id2).pop(node_id1)
             self.__edgeSize -= 1
-            self.__mc +=1
+            self.__mc += 1
             return True
         if node_id1 not in self.Nodes or node_id2 not in self.Nodes or node_id1 == node_id2:
             return False
 
+    def remove_node(self, node_id):
+        return True
 
-    def remove_node(self, node_id: int) -> bool:
-        return False
-
-    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+    def add_node(self, node_id, pos=None):
         if node_id not in self.Nodes:
-            self.Nodes[node_id]= NodeData(key = node_id , pos=pos)
-            self.Edges_in.__setitem__(node_id,{})
-            self.Edges_out.__setitem__(node_id,{})
-            self.__nodeSize+=1
-            self.__mc+=1
+            self.Nodes[node_id] = NodeData(key=node_id, pos=pos)
+            self.Edges_in.__setitem__(node_id, {})
+            self.Edges_out.__setitem__(node_id, {})
+            self.__nodeSize += 1
+            self.__mc += 1
             return True
         return False
-    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-        nodes =  self.get_all_v().keys()
-        if id1!=id2 and id1 in nodes and id2 in nodes and id2 not in self.Edges_out[id1] and weight >= 0:
+
+    def add_edge(self, id1, id2, weight):
+        nodes = self.Nodes.keys()
+        if id1 != id2 and id1 in nodes and id2 in nodes and id2 not in self.Edges_out[id1] and weight >= 0:
             self.Edges_in[id2][id1] = weight
-            self.Edges_out[id1][id2]= weight
-            self.__mc +=1
-            self.__edgeSize +=1
+            self.Edges_out[id1][id2] = weight
+            self.__mc += 1
+            self.__edgeSize += 1
             return True
         return False
-    def get_mc(self) -> int:
+
+    def get_mc(self):
         return self.__mc
 
-    def get_all_v(self) -> dict:
-        return self.Nodes
-
-
-    def all_in_edges_of_node(self, id1: int) -> dict:
-        if id1 not in self.Nodes:
-            return {}
-        return self.Edges_in.get(id1)
-
-    def all_out_edges_of_node(self, id1: int) -> dict:
-        if id1 not in self.Nodes:
-            return {}
-        return self.Edges_out.get(id1)
-
-    def e_size(self) -> int:
+    def e_size(self):
         return self.__edgeSize
 
-
-    def v_size(self) -> int:
+    def v_size(self):
         return len(self.Nodes)
