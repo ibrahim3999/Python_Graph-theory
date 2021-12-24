020cc11b5efa8e6c88ff7dff9956b4b601458834
ibrahim mjadly <48316131+ibrahim3999@users.noreply.github.com>
2021-12-24 22:07:03 +0200

    Update GraphAlgo.py

--- ClssesImp/GraphAlgo.py
+++ ClssesImp/GraphAlgo.py
@@ -1,14 +1,16 @@
 import json
+import sys
 from typing import List
-
+from queue import  PriorityQueue
 from DiGraph import DiGraph
+from NodeData import NodeData
 from src.GraphAlgoInterface import GraphAlgoInterface
 from src.GraphInterface import GraphInterface
-
+import heapq as hq
 
 class GraphAlgo(GraphAlgoInterface):
 
-    def __init__(self, G: DiGraph):
+    def __init__(self, G: DiGraph) -> object:
         self.__G = G
 
     def get_graph(self) -> GraphInterface:
@@ -49,88 +51,102 @@ class GraphAlgo(GraphAlgoInterface):
             return False
         finally:
             W.close()
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        min_tsp = []
+        nodes = self.get_graph().get_all_v()
+        for nodeStart in nodes:
+            for nodeForward in nodes:
+                if (nodeStart != nodeForward):
+                    min_tsp.append(self.shortest_path(nodeStart, nodeForward))
+        lstOfLst = [list(ele) for ele in min_tsp]
+        sortList = sorted(lstOfLst)
+        for i in range(len(sortList)):
+            if set(sorted(node_lst)).issubset(set(sorted(sortList[i][1]))):  # sortList[i][1].sort == node_lst.sort:
+                return sortList[i][1], sortList[i][0]
+
+        return [], -1
 
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
-        path = []
-        nodes = self.graph.get_all_v()
-        if id1 not in nodes or id2 not in nodes:
-            return  math.inf, path
-        if id1 == id2:
-            path.append(id2)
-            return 0, path
-        self.reset(nodes)
-        nodes = self.dijkstra(nodes, id1)
-        dist = nodes[id2].get_tag()
-        if dist == math.inf:
-            return math.inf, path
-        dest = id2
-        while dest != -1:
-            next_node = nodes[dest]
-            dest = next_node.get_parent()
-            path.insert(0, next_node.get_key())
-        return dest, path
-    def reset(self, nodes):
-        for node in nodes.values():
-            node.set_tag(math.inf)
-            node.set_info("no")
-            node.set_parent(None)
+        if self.get_graph().get_all_v().get(id1) is None or self.get_graph().get_all_v().get(id2) is None:
+            return float('inf'), []
+        self.Rrefsh()
+        shortest_path = self.rec(id1, id2, self.dijkstra(self.get_graph().get_all_v().get(id1), self.get_graph().get_all_v().get(id2)))
+        return self.get_graph().get_all_v().get(id2).get_dist(), shortest_path
 
-    def TSP(self, node_lst: List[int]) -> (List[int], float):
-        """
-        Finds the shortest path that visits all the nodes in the list
-        :param node_lst: A list of nodes id's
-        :return: A list of the nodes id's in the path, and the overall distance
-        """
+    def shortest_path_dist(self, id1: int, id2: int) -> (float, list):
+        if self.get_graph().get_all_v().get(id1) is None or self.get_graph().get_all_v().get(id2) is None:
+            return float('inf'), []
+        self.Rrefsh()
+        shortest_path = self.rec(id1, id2, self.dijkstra(self.get_graph().get_all_v().get(id1), self.get_graph().get_all_v().get(id2)))
+        return self.get_graph().get_all_v().get(id2).get_dist()
 
     def centerPoint(self) -> (int, float):
-        """
-        Finds the node that has the shortest distance to it's farthest node.
-        :return: The nodes id, min-maximum distance
-        """
+        MaxResults = {}
+        MinKey=-1
+        MinOfAll = float('inf')
+        nodes = self.__G.get_all_v()
+        for n in nodes:
+            max = -1
+            for n1 in nodes:
+                if(n!=n1):
+                    dist = self.shortest_path_dist(n,n1)
+                    if(dist!=float('inf')):
+                        if(max<dist):
+                            max=dist
+            MaxResults[n] = max
 
-       def plot_graph(self) -> None:
-        GraphFrame().draw_graph()
+        for key,val in MaxResults.items():
+            if(MaxResults.get(key)<MinOfAll):
+                MinOfAll=MaxResults.get(key)
+                MinKey=key
 
+        return MinKey,MinOfAll
 
-    def dijkstra(self, Nodes: dict, id1: int) -> list:
-            firstnode = Nodes[id1]
-            firstnode.ClssesImp.NodeData.set_parent(-1)
-            queue = PriorityQueue()
-            queue.put(firstnode)
-            firstnode
-            while not (queue.empty()):
-                vx = queue.get()
-                EdgesOut = self.graph.all_out_edges_of_node(vx.get_key())
-                for key, weight in edges_out.items():
-                    if vx.ClssesImp.NodeData.get_tag() + weight < nodes[key].ClssesImp.NodeData.get_tag():
-                        queue.put(nodes[key])
-                        nodes[key].ClssesImp.NodeData.set_tag(vx.ClssesImp.NodeData.get_tag()+weight)
-                        nodes[key].ClssesImp.NodeData.set_parent(vx.ClssesImp.NodeData.get_key())
+    def rec(self, src: int, dest: int, allPath: dict) -> list:
+        if allPath.get(dest) is None:
+            return []
+        short = [dest]
+        parent = allPath.get(dest)
+        while parent != src:
+            short.insert(0, parent)
+            parent = allPath.get(parent)
+        short.insert(0, src)
+        return short
 
-            return nodes
-
-
-
-
-
-
-    def __eq__(self, other):
-        if type(other) is not GraphAlgo:
-            return False
-        if self.__G.__eq__(GraphAlgo.get_graph(other)):
-            return True
-        return False
-
-
-
+    def dijkstra(self, src: NodeData, dest: NodeData) -> dict:
+        src.set_dist(0)
+        src.set_visited(True)
+        h: hq = []
+        hq.heappush(h, (0, src))
+        res= {}
+        while len(h) > 0:
+            n: NodeData = hq.heappop(h)[1]
+            if n.get_key() == dest.get_key():
+                break
+            if not n.get_visited():
+                n.set_visited(True)
+            for i in self.get_graph().all_out_edges_of_node(n.get_key()):
+                v: NodeData= self.get_graph().get_all_v().get(i)
+                if not v.get_visited():
+                    if v.get_dist() == -1 or n.get_dist() + self.get_graph().all_out_edges_of_node(n.get_key()).get(i) < v.get_dist():
+                        v.set_dist(n.get_dist() + self.get_graph().all_out_edges_of_node(n.get_key()).get(i))
+                        hq.heappush(h, (n.get_dist() + self.get_graph().all_out_edges_of_node(n.get_key()).get(i), v))
+                        res[v.get_key()] = n.get_key()
+        return res
+    def Rrefsh(self):
+        vertx: dict = self.get_graph().get_all_v()
+        for n in vertx:
+            vertx.get(n).set_visited(False)
+            vertx.get(n).set_dist(float('inf'))
 if __name__ == '__main__':
-    a = DiGraph()
-    b = GraphAlgo(a)
-
+    b=GraphAlgo(DiGraph())
     b.load_from_json(r"C:\Users\User\Desktop\Ex3\data\A0.json")
-    nodes = b.get_graph().e_size()
-    print(nodes)
-    x = "C:\\Users\\User\\Desktop\\Ex3\\data\\y.json"
-    b.save_to_json(x)
-    print(a.Nodes.keys())
+    ##print(b.get_graph().all_out_edges_of_node(0))
+    ##    x = "C:\\Users\\User\\Desktop\\Ex3\\data\\y.json"
+    ## b.save_to_json(x)
+   ## print(b.getMax())
+   ## print(b.centerPoint())
+    print(b.centerPoint())
+    print(b.TSP([0,1,3]))
+
