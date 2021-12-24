77b3ef6d0d01497a3fded82ee6801c61b7df5477
ibrahim mjadly <48316131+ibrahim3999@users.noreply.github.com>
2021-12-24 22:06:39 +0200

    Update DiGraph.py

--- ClssesImp/DiGraph.py
+++ ClssesImp/DiGraph.py
@@ -1,20 +1,26 @@
-from ClssesImp.NodeData import NodeData
+from NodeData import NodeData
 from src.GraphInterface import GraphInterface
 
 
 class DiGraph(GraphInterface):
+    Nodes = dict()
+    Edges_out = dict()
+    Edges_in = dict()
 
     def __init__(self):
-
         super()
         self.Nodes = dict()
         self.Edges_out = dict()
         self.Edges_in = dict()
+        self.Edges = dict(dict())
         self.__nodeSize = 0
         self.__edgeSize = 0
         self.__mc = 0
 
-    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+    def get_Node(self, i):
+        return self.Nodes.get(i)
+
+    def remove_edge(self, node_id1, node_id2):
 
         if node_id1 != node_id2 and node_id2 in self.Edges_out.get(node_id1):
             self.Edges_out.get(node_id1).pop(node_id2)
@@ -25,7 +31,7 @@ class DiGraph(GraphInterface):
         if node_id1 not in self.Nodes or node_id2 not in self.Nodes or node_id1 == node_id2:
             return False
 
-    def remove_node(self, node_id: int) -> bool:
+    def remove_node(self, node_id):
         if node_id in self.Nodes:
             for edges in list(self.all_out_edges_of_node(node_id).keys()):
                 self.remove_edge(node_id, edges)
@@ -40,7 +46,7 @@ class DiGraph(GraphInterface):
         else:
             return False
 
-    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+    def add_node(self, node_id, pos=None):
 
         if node_id not in self.Nodes:
             self.Nodes[node_id] = NodeData(key=node_id, pos=pos)
@@ -51,7 +57,7 @@ class DiGraph(GraphInterface):
             return True
         return False
 
-    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+    def add_edge(self, id1, id2, weight):
 
         nodes = self.get_all_v().keys()
         if id1 != id2 and id1 in nodes and id2 in nodes and id2 not in self.Edges_out[id1] and weight >= 0:
@@ -62,42 +68,26 @@ class DiGraph(GraphInterface):
             return True
         return False
 
-    def get_mc(self) -> int:
+    def get_mc(self):
         return self.__mc
 
-    def get_all_v(self) -> dict:
+    def get_all_v(self):
         return self.Nodes
 
-    def all_in_edges_of_node(self, id1: int) -> dict:
+    def all_in_edges_of_node(self, id1):
 
         if id1 not in self.Nodes:
             return {}
         return self.Edges_in.get(id1)
 
-    def all_out_edges_of_node(self, id1: int) -> dict:
+    def all_out_edges_of_node(self, id1):
 
         if id1 not in self.Nodes:
             return {}
         return self.Edges_out.get(id1)
 
-    def e_size(self) -> int:
+    def e_size(self):
         return self.__edgeSize
 
-    def v_size(self) -> int:
+    def v_size(self):
         return len(self.Nodes)
-
-    def __eq__(self, other):
-
-        if type(other) is not DiGraph:
-            return False
-        if self.e_size() != DiGraph.e_size(other):
-            return False
-        if self.v_size() != DiGraph.e_size(other):
-            return False
-        for node in self.Nodes.values():
-            if NodeData.get_key(node) not in DiGraph.get_all_v(other).keys():
-                return False
-            return True
-
-    def __repr__(self):
-        return f"Graph: |V|={self.v_size()} , |E|={self.e_size()}"
