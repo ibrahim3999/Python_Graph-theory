2873fee6d26e6d0466eb79782542761f72b4d0ef
ibrahim mjadly <48316131+ibrahim3999@users.noreply.github.com>
2021-12-24 22:07:31 +0200

    Update NodeData.py

--- ClssesImp/NodeData.py
+++ ClssesImp/NodeData.py
@@ -1,42 +1,74 @@
+import sys
 
 
 class NodeData:
-    def __init__(self, key: int, tag=0, pos: tuple = None):
-        self.__key: int = key
+    def __init__(self, key, tag=0, pos=None):
+        self.__key = key
         self.__tag = tag
         self.__info = "none"
         self.__parent = None
         self.__location = pos
+        self.__visited = False
+        self.__dist = float('inf')
+        self.__weight = 0
+        self.distance = float('inf')
 
-    def get_location(self) -> [tuple]:
+    def get_dist(self):
+        return self.__dist
+
+    def set_dist(self, d):
+        self.__dist = d
+
+    def get_visited(self):
+        return self.__visited
+
+    def set_visited(self, v):
+        if v == 1:
+            self.__visited = True
+        else:
+            self.__visited=False
+
+    def get_location(self):
         return self.__location
 
     def set_location(self, x, y, z):
         self.__location = (x, y, z)
 
-    def get_key(self) -> int:
+    def get_key(self):
         return self.__key
 
-    def set_key(self, key: int):
+    def set_key(self, key):
         self.__key = key
 
-    def get_tag(self) -> float:
+    def get_tag(self):
         return self.__tag
 
-    def set_tag(self, tag: int):
+    def set_tag(self, tag):
         self.__tag = tag
 
-    def get_parent(self) -> int:
+    def get_parent(self):
         return self.__parent
 
     def set_parent(self, parent):
         self.__parent = parent
 
-    def get_info(self) -> str:
+    def get_info(self):
         return self.__info
 
-    def set_info(self, info: str):
+    def set_info(self, info):
         self.__info = info
+
+    def get_weight(self):
+        return self.weight
+
+    def set_weight(self, w):
+        self.__weight = w;
+
+    def get_distance(self):
+        return self.distance
+
+    def set_distance(self, d):
+        self.distance = d
 
     def __repr__(self):
         if self.__location is None:
@@ -49,3 +81,9 @@ class NodeData:
 
     def __gt__(self, other):
         return self.__tag > other.__tag
+
+
+if __name__ == '__main__':
+    a = {0: 1, 1: 2, 2: 3}
+    nodes = a.keys()
+    print(nodes)
