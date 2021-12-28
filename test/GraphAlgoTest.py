a413cab342b8415145256a8c6b36ac7e16382a35
mohamedmagadlh <93886878+mohamedmagadlh@users.noreply.github.com>
2021-12-28 14:55:07 +0200

    Update GraphAlgoTest.py

--- test/GraphAlgoTest.py
+++ test/GraphAlgoTest.py
@@ -27,31 +27,54 @@ class GraphAlgoTest(unittest.TestCase):
         graph = DiGraph()
         return graph
 
+    def test_load_from_json(self):
+        graph = GraphAlgo()
+        self.assertTrue(graph.load_from_json("C:\Users\moham\PycharmProjects\Ex3\data\A2.json"))
+        self.assertTrue(graph.load_from_json("C:\Users\moham\PycharmProjects\Ex3\data\A1.json"))
+
+    def test_save_to_json(self):
+        graph = self.build_graph()
+        graphalgo = GraphAlgo()
+        GraphAlgo.__init__(graphalgo, graph)
+        self.assertTrue(graphalgo.save_to_json("check1"))
+
     def test_shortest_path(self):
         graph = self.build_graph()
-        a=DiGraph()
-        graph_a = GraphAlgo(a)
-        GraphAlgo.__init__(graph_a, graph)
+        graphalgo = GraphAlgo()
+        GraphAlgo.__init__(graphalgo, graph)
 
-        path_len = graph_a.shortest_path(2, 4).__getitem__(0)
-        path_list = graph_a.shortest_path(2, 4).__getitem__(1)
+        path_len = graphalgo.shortest_path(2, 4).__getitem__(0)
+        path_list = graphalgo.shortest_path(2, 4).__getitem__(1)
         expected_list = [2, 3, 4]
         self.assertEqual(4, path_len)
         self.assertEqual(3, len(path_list))
         self.assertEqual(expected_list.__str__(), path_list.__str__())
 
-        path_len = graph_a.shortest_path(1, 4).__getitem__(0)
-        path_list = graph_a.shortest_path(1, 4).__getitem__(1)
+        path_len = graphalgo.shortest_path(1, 4).__getitem__(0)
+        path_list = graphalgo.shortest_path(1, 4).__getitem__(1)
         expected_list = [1, 0, 4]
         self.assertEqual(5.5, path_len)
         self.assertEqual(3, len(path_list))
         self.assertEqual(expected_list.__str__(), path_list.__str__())
 
         graph = self.build_empty_graph()
-        graph_a.__init__(graph)
-        path_len = graph_a.shortest_path(1, 50).__getitem__(0)
-        path_list = graph_a.shortest_path(1, 50).__getitem__(1)
+        graphalgo.__init__(graph)
+        path_len = graphalgo.shortest_path(1, 50).__getitem__(0)
+        path_list = graphalgo.shortest_path(1, 50).__getitem__(1)
         self.assertEqual(math.inf, path_len)
         self.assertEqual([], path_list)
+
+    def test_TSP(self):
+        graph = self.build_graph()
+        G_algo = GraphAlgo(graph)
+        self.assertEqual(G_algo.TSP([1, 2, 4]))
+
+
+def test_center(self):
+        G_algo = GraphAlgo()
+        G_algo.load_from_json('C:\Users\moham\PycharmProjects\Ex3\data\A0.json')
+        G_center = G_algo.centerPoint()
+        self.assertEqual((8, 9.925289024973141), G_center)
+
 if __name__ == '__main__':
     unittest.main()
